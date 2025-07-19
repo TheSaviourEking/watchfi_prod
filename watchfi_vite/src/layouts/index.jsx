b8f32728d07cd74ed9aacc4cbc8f71ac7a01dee9
TheSaviourEking <saviour.eking@cyberservices.com>
2025-07-19 21:15:04 +0100

    add layouts

---
+++ watchfi_vite/src/layouts/index.jsx
@@ -0,0 +1,6 @@
+import RootLayout from "./RootLayout";
+import DashboardLayout from "./DashboardLayout";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export { RootLayout, DashboardLayout, Navbar, Footer }
